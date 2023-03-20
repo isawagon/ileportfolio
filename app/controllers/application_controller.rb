0f74ae662d74d01748cc9553b75c641f075a853c
Isabelle Levy <isabel.levy@icloud.com>
2023-03-20 11:16:20 +0100

    redirect heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: [:about]
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 end
