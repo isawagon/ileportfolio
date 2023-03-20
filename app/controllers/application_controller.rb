e57ae1d17a6c40cd2c11cd3143992c57075d3b3f
Isabelle Levy <isabel.levy@icloud.com>
2023-03-20 10:58:05 +0100

    https

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: [:about]
+  skip_before_action :verify_authenticity_token
 end
